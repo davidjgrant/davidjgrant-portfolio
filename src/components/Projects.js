f44ea06668638668c8280b1d897e663392eef0d8
David Grant <davidgrant132@gmail.com>
2021-01-03 19:03:57 +0000

    add first project template with data fetching from contentful.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,15 +1,10 @@
 import React from 'react'
-import { Link } from 'gatsby'
 
-const Projects = () => {
+
+const Projects = ({ children }) => {
     return (
         <div>
-            These are my projects:
-            <div>
-                <Link to="/Paperboat" title="Paperboat Shop Project">
-                    <h2>Paperboat</h2>
-                </Link>
-            </div>
+            {children}
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,43 @@
 import React from "react"
+import { Link, graphql } from 'gatsby'
 import HomepageLayout from "../components/HomepageLayout"
 import SEO from "../components/SEO"
 import About from "../components/About"
 import Footer from "../components/Footer"
 import Projects from "../components/Projects"
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <HomepageLayout>
       <SEO title="Home" />
       <About />
       <Footer />
-      <Projects />
+      <Projects>
+        {data.projects.nodes.map(project => (
+                <div key={`${project.slug}`}>
+                    <Link to={`/${project.slug}`}>
+                        <h2>{project.title}</h2>
+                        <img src={`${project.image.fluid.src}`} alt={`${project.image.description}`}/>
+                    </Link>
+                </div>
+            ))}
+      </Projects>
     </HomepageLayout>
   )
 }
+
+export const query = graphql`
+  {
+    projects: allContentfulPortfolio {
+        nodes {
+        title
+        image {
+            fluid {
+            src
+            }
+        }
+        slug
+        }
+    }
+  }
+`;
