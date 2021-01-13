2e9fc6dd3084518a7ed9d1c431249193bd700ff9
David Grant <davidgrant132@gmail.com>
2021-01-13 21:57:51 +0000

    remove AboutInfo component to tidy up

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,6 @@
 import React from "react"
-import { AboutInfo } from "./AboutInfo"
-import { AboutWrapper, AboutItems, ProfileImg, Logo, SocialIcons, SocialIconLink, ContactButtons, CVButton, ContactButton, LinkedIn, Dribbble, Github, Medium, Spotify } from "../elements"
+import { StaticQuery, graphql } from "gatsby"
+import { AboutWrapper, AboutItems, ProfileImg, Logo, Info, SocialIcons, SocialIconLink, ContactButtons, CVButton, ContactButton, LinkedIn, Dribbble, Github, Medium, Spotify } from "../elements"
 
 export const About = () => {
     return (
@@ -8,7 +8,28 @@ export const About = () => {
             <AboutItems>
                 <ProfileImg alt="Profile Picture" title="Profile Picture" />
                 <Logo alt="DavidJGrant Logo" title="DavidJGrant Logo" />
-                <AboutInfo />
+                <StaticQuery
+                    query={graphql`
+                    {
+                        allContentfulHomepageAbout {
+                        edges {
+                        node {
+                            description {
+                            description
+                            }
+                        }
+                        }
+                        }
+                    }
+                    `}
+                    render={data => (
+                    <div>
+                        {data.allContentfulHomepageAbout.edges.map(edge => (
+                                    <Info>{edge.node.description.description}</Info>
+                                ))}
+                    </div>
+                    )}
+                />
                 <SocialIcons>
                     <SocialIconLink href="https://www.linkedin.com/in/davidjohngrant/" target="_blank" title="LinkedIn Icon"><LinkedIn /></SocialIconLink>
                     <SocialIconLink href="/" target="_blank" title="Dribble Icon" ><Dribbble /></SocialIconLink>
