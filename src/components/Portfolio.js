89a88382b41ef2006579a54d808c0ee9ab714c54
David Grant <davidgrant132@gmail.com>
2021-01-24 17:15:21 +0000

    change staticQuery render to useStaticQuery

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react"
-import { graphql, StaticQuery, Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 import { PortfolioWrapper } from "../elements"
 import { PortfolioCard, CardImage } from "../components"
 
@@ -7,45 +7,35 @@ export const Portfolio = () => {
 
   const [color, setColor] = useState(`yellow`);
 
-return (
-  <StaticQuery
-
-    query={ graphql`
-      query PortfolioQuery {
-        projects: allContentfulPortfolio {
-          edges {
-            node {
-              title
-              image {
-                fluid(maxWidth: 350, resizingBehavior: FILL, quality: 100, cropFocus: CENTER) {
-                  ...GatsbyContentfulFluid  
-                }
+  const data = useStaticQuery(graphql`
+    query {
+      projects: allContentfulPortfolio {
+        edges {
+          node {
+            title
+            slug
+            image {
+              fluid {
+                ...GatsbyContentfulFluid
               }
-              slug
             }
           }
         }
       }
-    `}
+    }
+  `)
 
-        render={data => {
-
-          return (
-            <PortfolioWrapper style={{ backgroundColor: color }}>
-
-              {data.projects.edges.map(edge => (
-                  <PortfolioCard key={`${edge.node.slug}`} color="red" setColor={setColor}>
-                    <Link to={`/${edge.node.slug}`}>
-                      <CardImage fluid={edge.node.image.fluid} alt={`${edge.node.image.description}`}/>
-                      <h2>{edge.node.title}</h2>
-                    </Link>
-                  </PortfolioCard>
-              ))}
-            </PortfolioWrapper>
-          )
-          
-        }}
-    />
+return (
+    <PortfolioWrapper style={{ backgroundColor: color }}>
+      {data.projects.edges.map(edge => (
+        <PortfolioCard key={edge.node.slug} color="red" setColor={setColor}>
+          <Link to={`/${edge.node.slug}`}>
+            <CardImage fluid={edge.node.image.fluid} alt={edge.node.image.description}/>
+            <h2>{edge.node.title}</h2>
+          </Link>
+        </PortfolioCard>
+      ))}
+    </PortfolioWrapper>
   )
 }
 
