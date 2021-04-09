6eb3f9d56f97592b07153aee70b019ec230fbffa
David Grant <davidgrant132@gmail.com>
2021-04-09 19:19:00 +0100

    Links switch in footer

--- src/components/HomepageFooter.js
+++ src/components/HomepageFooter.js
@@ -1,18 +1,22 @@
 import React from "react"
+import { useLocation } from "@reach/router"
 import { HomepageFooterWrapper, FooterItems, FooterItem, FooterLinks, FooterLink } from "../elements"
 
 export const HomepageFooter = () => {
 
     const currentYear = new Date();
     const year = currentYear.getFullYear();
+    const location = useLocation();
+
+    
 
     return (
         <HomepageFooterWrapper>
             <FooterItems>
                 <FooterItem>© {year} <FooterLink to="/">DavidJGrant</FooterLink></FooterItem>
                 <FooterLinks>
-                    <FooterLink to="/AboutPage">About Me</FooterLink>
-                    <FooterLink to="/ContactPage">Contact Me</FooterLink>
+                    {location.pathname !== '/AboutPage' && (<FooterLink to="/AboutPage">About Me</FooterLink>)}
+                    {location.pathname !== '/ContactPage' && (<FooterLink to="/ContactPage">Contact Me</FooterLink>)}
                 </FooterLinks>
             </FooterItems>       
         </HomepageFooterWrapper>
