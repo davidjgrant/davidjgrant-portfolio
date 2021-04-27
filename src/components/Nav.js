68dbd926ff6e0d10de93c64ae932454c8a340bbe
David Grant <davidgrant132@gmail.com>
2021-04-27 19:51:48 +0100

    add public folder

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-// import { FaLink } from 'react-icons/fa';
-// import { CgDarkMode } from "react-icons/cg";
+import { FaLink } from 'react-icons/fa';
+import { CgDarkMode } from "react-icons/cg";
 import { NavWrapper, NavLogo, IconsWrapper } from "../elements"
 
 export const Nav = () => {
@@ -24,8 +24,8 @@ export const Nav = () => {
                 <img src={data.contentfulHomepageAbout.logo.fluid.src} alt="DavidJGrant Logo" />
             </NavLogo>
             <IconsWrapper>
-                {/*<FaLink onClick={() =>  navigator.clipboard.writeText(url)} title="Copy URL to Clipboard" />
-                 <CgDarkMode title="Switch to Dark Mode" /> */}
+                <FaLink onClick={() =>  navigator.clipboard.writeText('/')} title="Copy URL to Clipboard" />
+                 <CgDarkMode title="Switch to Dark Mode" />
             </IconsWrapper>
         </NavWrapper>
     )
